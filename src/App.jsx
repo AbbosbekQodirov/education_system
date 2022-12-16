0b6edcc0aa77fa42508095ec04780c7358a9a6de
Raximov SHaxzodbek <raximovshaxzodbek2002@gmail.com>
2022-12-17 00:30:16 +0500

    test page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Test Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quia
+        ipsa quis quaerat est expedita qui magnam officia voluptas deleniti
+        unde, quos doloribus sit, numquam iste porro ipsam cum doloremque.
+      </p>
+    </div>
+  );
+}
+
+export default App;
